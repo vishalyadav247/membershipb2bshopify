d2426d258dafda96d34b15bf2322217812503a54
Abhinav <abhinav@progryss.com>
2024-10-07 13:45:53 +0530

    First Commit

--- server/index.js
+++ server/index.js
@@ -5,7 +5,10 @@ const connectDb = require('./utils/db');
 const router = require('./router/auth-routes')
 
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+    origin:'http://localhost:3000',
+    credentials:true
+}))
 app.use("/api/",router)
 
 connectDb().then(() => {
